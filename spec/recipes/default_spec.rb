c0722bd9559f2269b5bf6d8767294d442eefd048
James Awesome <jamestheawesome@gmail.com>
2014-11-04 23:35:58 -0500

    Correct version in default spec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -3,7 +3,7 @@ require 'fauxhai'
 
 describe 'base::default' do 
   let(:chef_run) do 
-    ChefSpec::SoloRunner.new(platform:'ubuntu', version:'12.04') do |node| 
+    ChefSpec::SoloRunner.new(platform:'ubuntu', version:'14.04') do |node| 
       node.name('test-node.jamestheaweso.me')
     end.converge(described_recipe)
   end
